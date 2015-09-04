b01e174068d3d3f252038ae5343df8b3437e09dd
Robson Peixoto <robsonpeixoto@gmail.com>
2015-09-04 09:01:09 -0300

    Using tsuru's hipache

--- recipes/hipache.rb
+++ recipes/hipache.rb
@@ -1,37 +1,13 @@
- package 'nodejs' do
+include_recipe 'tsuru::repo'
+
+package 'node-hipache' do
   action :install
-end
-
-package 'npm' do
-  action :install
-  not_if 'test -x /usr/bin/npm' # some distributions include npm with nodejs package
-end
-
-execute 'install hipache' do
-  action :run
-  user 'root'
-  command 'npm install hipache -g'
-end
-
-link '/usr/bin/hipache' do
-  action :create
-  to '/usr/local/bin/hipache'
-  only_if 'test -e /usr/local/bin/hipache'
-  not_if 'test -e /usr/bin/hipache'
 end
 
 directory node['hipache']['server']['accessLog'].gsub(/\/[^\/]+$/,'') do
   action :create
   owner node['hipache']['user']
   recursive true
-end
-
-cookbook_file '/etc/init/hipache.conf' do
-  action :create
-  owner 'root'
-  mode 0644
-  source 'hipache.conf.init'
-  notifies :start, 'service[hipache]'
 end
 
 file '/etc/hipache.conf' do

d0cc0f3bb7abbfd60060801d1200bae913f3aaca
Robson Peixoto <robsonpeixoto@gmail.com>
2015-09-03 22:23:31 -0300

    Remove all deprecated configurations

--- attributes/server.rb
+++ attributes/server.rb
@@ -1,28 +1,30 @@
+default['tsuru']['cname'] = node['fqdn']
+
+## HTTP server
 default['tsuru']['server']['listen'] = '0.0.0.0:8080'
 # default['tsuru']['server']['use-tls'] = false
-# default['tsuru']['server']['tls-cert-file'] = nil # path to cert
-# default['tsuru']['server']['tls-key-file'] = nil # path to key
-default['tsuru']['server']['cname'] = node['fqdn']
-default['tsuru']['server']['host'] = "http#{'s' if node['tsuru']['server']['use-tls']}://#{node['tsuru']['server']['cname']}:#{node['tsuru']['server']['listen'][/\d+$/]}"
-default['tsuru']['server']['admin-team'] = 'admin'
-# default['tsuru']['server']['debug'] = false
-## when log:file is unset, tsuru logs to syslog
-#default['tsuru']['server']['log']['file'] = '/var/log/tsuru.log'
+# default['tsuru']['server']['tls']['cert-file'] = nil # path to cert
+# default['tsuru']['server']['tls']['key-file'] = nil # path to key
+default['tsuru']['server']['host'] = "http#{'s' if node['tsuru']['server']['use-tls']}://#{node['tsuru']['cname']}:#{node['tsuru']['server']['listen'][/\d+$/]}"
+# default['tsuru']['server']['server']['read-timeout'] = 0
+# default['tsuru']['server']['server']:['write-timeout'] = 0
+# default['tsuru']['server']['disable-index-page'] = false
+# default['tsuru']['server']['index-page-template']
 
 ## Database acess
 default['tsuru']['server']['database']['url'] = '127.0.0.1:27017'
 default['tsuru']['server']['database']['name'] = 'tsuru'
+# default['tsuru']['server']['database']['logdb-url'] = nil
+# default['tsuru']['server']['database']['logdb-name'] = nil
 
 ## Email configuration
 # default['tsuru']['server']['smtp']['server'] = nil
 # default['tsuru']['server']['smtp']['user'] = nil
 # default['tsuru']['server']['smtp']['password'] = nil
 
-## Git configuration
-default['tsuru']['server']['git']['unit-repo'] = '/home/application/current'
+## Repository configuration
+# default['tsuru']['server']['repo-manager'] = "gandalf"
 default['tsuru']['server']['git']['api-server'] = "http://#{node['gandalf-server']['host']}:8000"
-#default['tsuru']['server']['git']['rw-host'] = node['fqdn']
-#default['tsuru']['server']['git']['ro-host'] = node['fqdn']
 
 ## Authentication configuration
 default['tsuru']['server']['auth']['scheme'] = 'native' #only other option is oauth
@@ -43,37 +45,64 @@ default['tsuru']['server']['auth']['hash-cost'] = 10
 ## Queue configuration
 default['tsuru']['server']['queue']['mongo-url'] = '127.0.0.1:27017'
 default['tsuru']['server']['queue']['mongo-database'] = 'redis'
-
-## PubSub configuration
 default['tsuru']['server']['pubsub']['redis-host'] = '127.0.0.1'
 default['tsuru']['server']['pubsub']['redis-port'] = 'queuedb'
-# default['tsuru']['server']['redis-queue']['password'] = '' # blank for no password
-# default['tsuru']['server']['redis-queue']['db'] = 3
+# default['tsuru']['server']['pubsub']['redis-password'] = nil
+# default['tsuru']['server']['pubsub']['redis-db'] = nil
+# default['tsuru']['server']['pubsub']['pool-max-idle-conn'] = nil
+# default['tsuru']['server']['pubsub']['pool-idle-timeout'] = nil
+# default['tsuru']['server']['pubsub']['redis-dial-timeout'] = nil
+# default['tsuru']['server']['pubsub']['redis-read-timeout'] = nil
+# default['tsuru']['server']['pubsub']['redis-write-timeout'] = nil
+
+## Admin users
+default['tsuru']['server']['admin-team'] = 'admin'
 
 ## Quota management
 # default['tsuru']['server']['quota']['units-per-app'] = 'unlimited'
 # default['tsuru']['server']['quota']['apps-per-user'] = 'unlimited'
 
-## Hipache
+## Logging
+default['tsuru']['server']['debug'] = false
+## when log:file is unset, tsuru logs to syslog
+#default['tsuru']['server']['log']['file'] = '/var/log/tsuru.log'
+#default['tsuru']['server']['log']['disable-syslog'] = false
+#default['tsuru']['server']['log']['syslog-tag'] = 'tsr'
+#default['tsuru']['server']['log']['use-stderr'] = false
+
+## Routers - http://docs.tsuru.io/en/stable/reference/config.html#routers
+## using hipache as default
 default['tsuru']['server']['routers']['hipache']['type'] = 'hipache'
 default['tsuru']['server']['routers']['hipache']['domain'] = node['tsuru']['server']['cname']
 default['tsuru']['server']['routers']['hipache']['redis-server'] = '127.0.0.1:6379'
 
-## Docker provisioner
+## Provisioner
 default['tsuru']['server']['provisioner'] = 'docker'
+
+## Docker provisioner
 default['tsuru']['server']['docker']['collection'] = 'docker_containers'
+# default['tsuru']['server']['docker']['port-allocator'] = 'docker'
+# default['tsuru']['server']['docker']['registry'] = "#{node.fqdn}:5000"
+# default['tsuru']['server']['docker']['registry']['registry-max-try'] = nil
+# default['tsuru']['server']['docker']['registry-auth']['username'] = nil
+# default['tsuru']['server']['docker']['registry-auth']['password'] = nil
+# default['tsuru']['server']['docker']['registry-auth']['email'] = nil
 default['tsuru']['server']['docker']['repository-namespace'] = 'tsuru'
+# default['tsuru']['server']['docker']['bs']['image'] = 'tsuru/bs:v1'
+default['tsuru']['server']['docker']['bs']['socket'] = '/var/run/docker.sock'
+# default['tsuru']['server']['docker']['bs']['syslog-port'] = 1514
+# default['tsuru']['server']['docker']['max-workers'] = 0
 default['tsuru']['server']['docker']['router'] = 'hipache'
 default['tsuru']['server']['docker']['deploy-cmd'] = '/var/lib/tsuru/deploy'
+# default['tsuru']['server']['docker']['security-opts'] = nil # http://docs.tsuru.io/en/stable/reference/config.html#docker-security-opts
+# default['tsuru']['server']['docker']['scheduler']['total-memory-metadata'] = nil
+# default['tsuru']['server']['docker']['scheduler']['max-used-memory'] = nil
+default['tsuru']['server']['docker']['cluster']['storage'] = 'mongodb'
 default['tsuru']['server']['docker']['cluster']['mongo-url'] = '127.0.0.1:27017'
 default['tsuru']['server']['docker']['cluster']['mongo-database'] = 'cluster'
 default['tsuru']['server']['docker']['run-cmd']['bin'] = '/var/lib/tsuru/start'
 default['tsuru']['server']['docker']['run-cmd']['port'] = '8888'
-default['tsuru']['server']['docker']['ssh']['add-key-cmd'] = '/var/lib/tsuru/add-key'
 default['tsuru']['server']['docker']['ssh']['user'] = 'ubuntu'
-# default['tsuru']['server']['docker']['registry'] = "#{node.fqdn}:5000"
-# default['tsuru']['server']['docker']['servers'] = ['http://localhost:2375']
-
 #default['tsuru']['server']['docker']['healing']['heal-nodes'] = false
 #default['tsuru']['server']['docker']['healing']['active-monitoring-interval'] = 0
 #default['tsuru']['server']['docker']['healing']['disabled-time'] = 30
@@ -82,12 +111,14 @@ default['tsuru']['server']['docker']['ssh']['user'] = 'ubuntu'
 #default['tsuru']['server']['docker']['healing']['heal-containers-timeout'] = 0
 #default['tsuru']['server']['docker']['healing']['events_collection'] = 'healing_events'
 #default['tsuru']['server']['docker']['healthcheck']['max-time'] = 120
+#default['tsuru']['server']['docker']['image-history-size'] = 10
+#default['tsuru']['server']['docker']['auto-scale']['enabled'] = false
+#default['tsuru']['server']['docker']['auto-scale']['wait-new-time'] = 300
+#default['tsuru']['server']['docker']['auto-scale']['group-by-metadata'] = nil
+#default['tsuru']['server']['docker']['auto-scale']['metadata-filter'] = nil
+#default['tsuru']['server']['docker']['auto-scale']['max-container-count'] = nil
+#default['tsuru']['server']['docker']['auto-scale']['prevent-rebalance'] = nil
+#default['tsuru']['server']['docker']['auto-scale']['run-interval'] = 3600
+#default['tsuru']['server']['docker']['auto-scale']['scale-down-ratio'] = 1.33
 
-## IaaS configuration
-# default['tsuru']['server']['iaas']['default'] = nil
-# default['tsuru']['server']['iaas']['api-key'] = nil
-# default['tsuru']['server']['iaas']['secret-key'] = nil
-# default['tsuru']['server']['iaas']['url'] = nil
-# default['tsuru']['server']['iaas']['node-protocol'] = nil
-# default['tsuru']['server']['iaas']['node-port'] = nil
-# default['tsuru']['server']['iaas']['collection'] = nil
+## IaaS configuration - http://docs.tsuru.io/en/stable/reference/config.html#iaas-configuration
